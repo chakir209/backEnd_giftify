cb0b95a672c2849502691c6b3d91ffeec777a8a8
Aimad Benzakry <aimadbenzakry@gmail.com>
2023-10-22 18:05:12 +0100

    reviews app added

--- backend_gftify/urls.py
+++ backend_gftify/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('users.urls')),
+    path('api/', include('reviews.urls')),
     path('api/products/', test.liste_products),
     path('api/products/<int:pk>/', test.detail_products),
 ]
